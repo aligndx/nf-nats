b769d6ede1ca7d01f960d12d1652186f28b0a36b
mshunjan <manjothunjan@gmail.com>
2024-06-24 22:01:54 -0400

    Patch bug with events config

--- plugins/nf-nats/src/main/nextflow/nats/NatsFactory.groovy
+++ plugins/nf-nats/src/main/nextflow/nats/NatsFactory.groovy
@@ -32,7 +32,7 @@ class NatsFactory implements TraceObserverFactory {
         def isEnabled = session.config.navigate('nats.enabled') as Boolean
         def url = session.config.navigate('nats.url') as String
         def subject = session.config.navigate('nats.subject') as String
-        def events = session.config.navigate('nats.events') as List<String> ?: ['flow.start', 'process.start', 'process.complete', 'flow.error', 'flow.complete', 'process.logs']
+        def events = session.config.navigate('nats.events') as List<String> ?: ['workflow.start', 'workflow.error', 'workflow.complete', 'process.start', 'process.complete', 'process.logs']
         def jetstream = session.config.navigate('nats.jetstream') as Boolean ?: false
 
         def result = new ArrayList()
