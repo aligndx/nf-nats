d224ee16a43ffe210518d0e70199c5f383e7dc2e
mshunjan <manjothunjan@gmail.com>
2024-07-29 16:53:54 -0400

    refactor(NatsObserver.groovy): clean up observer code

--- plugins/nf-nats/src/main/nextflow/nats/NatsObserver.groovy
+++ plugins/nf-nats/src/main/nextflow/nats/NatsObserver.groovy
@@ -1,19 +1,3 @@
-/*
- * Copyright 2021, Seqera Labs
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package nextflow.nats
 
 import groovy.transform.CompileStatic
@@ -45,38 +29,41 @@ class NatsObserver implements TraceObserver {
     private boolean useJetStream
 
     NatsObserver(String natsUrl, String subject, Set<String> eventsToPublish, boolean useJetStream) {
-        // Connect to NATS
-        this.nc = Nats.connect(natsUrl)
+        this.natsUrl = natsUrl
         this.subject = subject
         this.eventsToPublish = eventsToPublish
         this.useJetStream = useJetStream
-
-        if (useJetStream) {
-            this.js = nc.jetStream()
-        }
+        connectToNats()
     }
 
+    private void connectToNats() {
+        nc = Nats.connect(natsUrl)
+        if (useJetStream) {
+            js = nc.jetStream()
+        }
+    }
+    
     void publishEvent(String eventType, String message) {
         if (eventsToPublish.contains(eventType)) {
             String subSubject = "${subject}.events.${eventType}"
             String formattedMessage = createJsonMessage(eventType, message)
-            if (useJetStream) {
-                js.publish(subSubject, formattedMessage.bytes)
-            } else {
-                nc.publish(subSubject, formattedMessage.bytes)
-            }
+            sendMessage(subSubject, formattedMessage)
         }
-    }
+    } 
 
     void publishLog(String logType, String message) {
         if (eventsToPublish.contains("process.logs")) {
             String subSubject = "${subject}.logs.${logType}"
             String formattedMessage = createJsonMessage(logType, message)
-            if (useJetStream) {
-                js.publish(subSubject, formattedMessage.bytes)
-            } else {
-                nc.publish(subSubject, formattedMessage.bytes)
-            }
+            sendMessage(subSubject, formattedMessage)
+        }
+    }
+
+    private void sendMessage(String subSubject, String message) {
+        if (useJetStream) {
+            js.publish(subSubject, message.bytes)
+        } else {
+            nc.publish(subSubject, message.bytes)
         }
     }
 
@@ -114,35 +101,26 @@ class NatsObserver implements TraceObserver {
 
     void publishLogs(TaskHandler handler) {
         Path workDir = handler.task.workDir
-
         if (workDir == null) {
             log.warn("Task '${handler.task.name}' workDir is null")
             return
         }
 
-        try {
-            log.debug("Checking logs for task: '${handler.task.name}' in directory: ${workDir}")
+        processLogFiles(handler, workDir, ".command.out", "stdout")
+        processLogFiles(handler, workDir, ".command.err", "stderr")
+    }
 
-            Path commandOutPath = workDir.resolve(".command.out")
-            Path commandErrPath = workDir.resolve(".command.err")
-
-            if (Files.exists(commandOutPath)) {
-                log.debug("Found .command.out for task: '${handler.task.name}'")
-                String commandOut = new String(Files.readAllBytes(commandOutPath))
-                publishLog("logs.stdout", "Task '${handler.task.name}' : ${commandOut}")
-            } else {
-                log.debug("No .command.out found for task: '${handler.task.name}'")
+    private void processLogFiles(TaskHandler handler, Path workDir, String fileName, String logType) {
+        Path filePath = workDir.resolve(fileName)
+        if (Files.exists(filePath)) {
+            try {
+                String content = new String(Files.readAllBytes(filePath))
+                publishLog(logType, "Task '${handler.task.name}': ${content}")
+            } catch (IOException e) {
+                log.error("Error reading file ${fileName} for task '${handler.task.name}'", e)
             }
-
-            if (Files.exists(commandErrPath)) {
-                log.debug("Found .command.err for task: '${handler.task.name}'")
-                String commandErr = new String(Files.readAllBytes(commandErrPath))
-                publishLog("logs.stderr", "Task '${handler.task.name}' : ${commandErr}")
-            } else {
-                log.debug("No .command.err found for task: '${handler.task.name}'")
-            }
-        } catch (IOException e) {
-            log.error("Error reading log files for task '${handler.task.name}'", e)
+        } else {
+            log.debug("No ${fileName} found for task: '${handler.task.name}'")
         }
     }
 }
